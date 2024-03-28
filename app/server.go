773f9bdd6bcc39a919843760e577a0362b06aa64
Kaialan <satyajit1501@gmail.com>
2024-03-28 21:01:04 +0530

    3rd stage commit

--- app/server.go
+++ app/server.go
@@ -2,35 +2,49 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func ParseRequest(request string) (string, string, string) {
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	requestFirstLine := strings.Split(request, "\r\n")[0]
+	requestParams := strings.Split(requestFirstLine, " ")
+
+	method := requestParams[0]
+	path := requestParams[1]
+	version := requestParams[2]
+
+	return method, path, version
+}
+
+func main() {
+
+	fmt.Println("Logging...")
+
+	//binding to port
+	listner, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-
-	defer conn.Close()
+	conn, err := listner.Accept()
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
+	defer conn.Close()
+
 	fmt.Println("Client connected")
+
+	//reading incomig requests
 	readBuffer := make([]byte, 2048)
 	bytesReceived, err := conn.Read(readBuffer)
+
 	if err != nil {
 		fmt.Printf("Error reading request: %v\n", err)
 		os.Exit(1)
@@ -38,12 +52,36 @@ func main() {
 
 	fmt.Printf("Read %d bytes from client\n", bytesReceived)
 
+	request := string(readBuffer[:bytesReceived])
+
+	//Paresing Request and Responsing with proper response
+
+	method, path, _ := ParseRequest(request)
+
 	httpResponse := "HTTP/1.1 200 OK\r\n\r\n"
-	bytesSent, err := conn.Write([]byte(httpResponse))
-	if err != nil {
-		fmt.Println("Error sending response: ", err.Error())
-		os.Exit(1)
+	defaultResponse := "HTTP/1.1 404 Not Found\r\n\r\n"
+
+	if method == "GET" {
+
+		switch path {
+		case "/":
+			bytesSent, err := conn.Write([]byte(httpResponse))
+
+			if err != nil {
+				fmt.Println("Error sending response: ", err.Error())
+				os.Exit(1)
+			}
+			fmt.Printf("Sent %d bytes to client (expected: %d)\n", bytesSent, len(httpResponse))
+		default:
+			_, err := conn.Write([]byte(defaultResponse))
+
+			if err != nil {
+				fmt.Println("Error sending response: ", err.Error())
+				os.Exit(1)
+			}
+		}
+	} else {
+		fmt.Println("Not a GET request")
 	}
 
-	fmt.Printf("Sent %d bytes to client (expected: %d)\n", bytesSent, len(httpResponse))
 }
